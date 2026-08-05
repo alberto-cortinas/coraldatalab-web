bea1307ec9f7279a8dae3b998167e5ce4faa6192
alberto <alberto@albertos-MacBook-Pro.local>
2026-08-05 10:27:52 +0200

    Simplify logo to offset aperture mark

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -5,10 +5,7 @@ export function Brand({ footer = false }) {
       href="#inicio"
       aria-label={footer ? undefined : 'Coral Data Lab, inicio'}
     >
-      <span className="brand__mark" aria-hidden="true">
-        <span className="brand__trunk" />
-        <i /><i /><i /><i /><i />
-      </span>
+      <span className="brand__mark" aria-hidden="true" />
       <span className="brand__name">
         <span className="brand__coral">CORAL</span>
         <span className="brand__rest">DATA LAB</span>
