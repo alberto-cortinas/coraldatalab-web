e10d9ee873199e6a26fdd6b6b131298aed3d102f
alberto <alberto@albertos-MacBook-Pro.local>
2026-08-05 10:15:30 +0200

    Redesign brand mark and editorial wordmark

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -6,12 +6,13 @@ export function Brand({ footer = false }) {
       aria-label={footer ? undefined : 'Coral Data Lab, inicio'}
     >
       <span className="brand__mark" aria-hidden="true">
-        <i />
-        <i />
-        <i />
-        <b />
+        <span className="brand__trunk" />
+        <i /><i /><i /><i /><i />
       </span>
-      <span className="brand__name">Coral Data <span>Lab</span></span>
+      <span className="brand__name">
+        <span className="brand__coral">CORAL</span>
+        <span className="brand__rest">DATA LAB</span>
+      </span>
     </a>
   )
 }
